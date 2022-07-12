22b225a7a72ae84415130c04eaf53739e123c5bd
Franco Olivera Reid <foliverareid@gmail.com>
2022-07-12 19:14:02 -0300

    agregamos array

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
+let montoCompra=parseInt(prompt("Monto de la compra"));
+let preguntaCuotas=parseInt(prompt("Cantidad de cuotas"));
+let alicuota=parseInt(prompt("Ingrese la alícuota de IVA que le corresponda (21, 10.5 o 0)"));
+
 function calcularCuotas (monto, cuotas) {
       
     let totalMes = 0;
     if (monto > 0) {
         totalMes = monto / cuotas;
     }
-    let totalMesMasIVA = totalMes + (totalMes * 0.21);
+    const tasasIVA = [0, 10.5, 21];
+    let totalMesMasIVA = 0;
+    if (tasasIVA.includes(alicuota)){
+        totalMesMasIVA = totalMes + (totalMes * (alicuota/100));
+    } else {
+        alert("Error al ingresar la alícuota")
+    }
     return totalMesMasIVA;
 }
+console.log(calcularCuotas(montoCompra, preguntaCuotas));
